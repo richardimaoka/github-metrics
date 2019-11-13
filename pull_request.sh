eb3c39d8dfc7f5a827111f59301648c1308389c7
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-11-13 14:16:34 +0900

    add pull_request.sh

---
+++ pull_request.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# cd to the current directory as it runs other shell scripts
+cd "$(dirname "$0")" || exit
+
+# -r flag to strip the double quotes
+GIHUB_TOKEN=$(cat env.json | jq -r '.github_token')
+GITHUB_ORGANIZATION=$(cat env.json | jq  -r '.github_organization')
+GITHUB_REPOSITORY=$(cat env.json | jq  -r '.github_repository')
+
+curl \
+  -H "Authorization: token ${GIHUB_TOKEN}" \
+  "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}" \
+  > "${ISSUE_NUMBER}.json"
