61b5a498e3af92dc2f3e6408b3d6d90358ea8eae
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-11-13 13:40:47 +0900

    sort params

--- issue.sh
+++ issue.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ISSUE_NUMBER=$1
-GIHUB_TOKEN=$2
-ORG_REPO=$3
+GIHUB_TOKEN=$1
+ORG_REPO=$2
+ISSUE_NUMBER=$3
 
 curl -H "Authorization: token ${GIHUB_TOKEN}" "https://api.github.com/repos/${ORG_REPO}/issues/${ISSUE_NUMBER}"
