14ef84b2e4df32decc64c61a115821fb80d59bf3
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-11-20 04:43:37 +0900

    Use 'jq env.json' instead of 'cat env.json | jq'

--- pulls.sh
+++ pulls.sh
@@ -4,9 +4,9 @@
 cd "$(dirname "$0")" || exit
 
 # -r flag to strip the double quotes
-GIHUB_TOKEN=$(cat env.json | jq -r '.github_token')
-GITHUB_ORGANIZATION=$(cat env.json | jq  -r '.github_organization')
-GITHUB_REPOSITORY=$(cat env.json | jq  -r '.github_repository')
+GIHUB_TOKEN=$(jq -r '.github_token' env.json)
+GITHUB_ORGANIZATION=$(jq  -r '.github_organization' env.json)
+GITHUB_REPOSITORY=$(jq  -r '.github_repository' env.json)
 
 curl \
   -H "Authorization: token ${GIHUB_TOKEN}" \
--- pulls_expand_all.sh
+++ pulls_expand_all.sh
@@ -3,7 +3,7 @@
 # cd to the current directory as it runs other shell scripts
 cd "$(dirname "$0")" || exit
 
-PULL_REQUESTS=$(cat pulls.json | jq '.[].number')
+PULL_REQUESTS=$(jq '.[].number' pulls.json)
 for PULL_REQUEST in $PULL_REQUESTS
 do
   ./pull_get_each.sh ${PULL_REQUEST}
