9c5ce99bfa7e168ceab6befa7730f2b1164968c2
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-11-20 07:47:26 +0900

    use latest pull req no. in the loop

--- pulls_expand_all.sh
+++ pulls_expand_all.sh
@@ -3,8 +3,16 @@
 # cd to the current directory as it runs other shell scripts
 cd "$(dirname "$0")" || exit
 
-PULL_REQUESTS=$(jq '.[].number' pulls.json)
-for PULL_REQUEST in $PULL_REQUESTS
+# GitHub API's 'List pull requests' endpoint (https://developer.github.com/v3/pulls/#list-pull-requests)
+# only returns 30 pull requests.
+# So we are using the following "brute-force" while loop to find all pull requests up to the latest one.
+PULL_REQUEST_LATEST=$(./pull_latest.sh | jq -r '.number')
+
+# brute-force: increment from 1 to latest
+PULL_REQUEST_NUMBER=1
+while [ "${PULL_REQUEST_NUMBER}" -lt "${PULL_REQUEST_LATEST}" ]
 do
-  ./pull_get_each.sh ${PULL_REQUEST}
+  echo "${PULL_REQUEST_NUMBER}"
+  ./pull_get_each.sh "${PULL_REQUEST_NUMBER}"
+  PULL_REQUEST_NUMBER=$((PULL_REQUEST_NUMBER + 1))
 done
