6cb91ce390366e9dd86a86061b0cdeffa14f9187
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-11-20 07:11:59 +0900

    handle error cases

--- pull_get_each.sh
+++ pull_get_each.sh
@@ -3,24 +3,38 @@
 # cd to the current directory as it runs other shell scripts
 cd "$(dirname "$0")" || exit
 
+PULL_REQUEST_NUMBER=$1
+
 # -r flag to strip the double quotes
 GIHUB_TOKEN=$(jq -r '.github_token' env.json)
 GITHUB_ORGANIZATION=$(jq  -r '.github_organization' env.json)
 GITHUB_REPOSITORY=$(jq  -r '.github_repository' env.json)
 
-PULL_REQUEST_NUMBER=$1
-
 mkdir -p pulls
 
-# 2>/dev/null to discard curl's in-progress stats
-JSON=$(curl \
-  -H "Authorization: token ${GIHUB_TOKEN}" \
-  "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/pulls/${PULL_REQUEST_NUMBER}" 2>/dev/null)
+while true
+do
+  JSON_FILENAME="pulls/${PULL_REQUEST_NUMBER}.json"
+  
+  # 2>/dev/null to discard curl's in-progress stats
+  curl \
+    -H "Authorization: token ${GIHUB_TOKEN}" \
+    "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/pulls/${PULL_REQUEST_NUMBER}" 2>/dev/null \
+    > "${JSON_FILENAME}"
+  
+  ERROR_MESSAGE=$(jq -r '.message' "${JSON_FILENAME}")
+  if [ "${ERROR_MESSAGE}" = "Not Found" ]; then
+    >&2 echo "ERROR: Not found. ${PULL_REQUEST_NUMBER} is most likely an issue, not a pull request."
+    rm "${JSON_FILENAME}"
+    exit 1
 
-ERROR_MESSAGE=$(echo "${JSON}" | jq -r '.message')
-if [ "${ERROR_MESSAGE}" = "Not Found" ] ; then
-  >&2 echo "Not found, pull request ${PULL_REQUEST_NUMBER}"
-  exit 1
-else
-  echo "${JSON}"> pulls/"${PULL_REQUEST_NUMBER}.json"
-fi
+  # TOO bad!! jq returns "null" = 'n'+'u'+'l'+'l' String for a non-existent value https://github.com/stedolan/jq/issues/354
+  elif [ "${ERROR_MESSAGE}" != "null" ]; then
+    >&2 echo "ERROR: ${ERROR_MESSAGE}"
+    exit 1
+
+  else
+    # Successful
+    exit 0
+  fi
+done
