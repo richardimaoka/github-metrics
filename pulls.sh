ce5fbf2423ce1afd17763a42d1b0e628da4e9e04
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-11-20 03:42:53 +0900

    Fix pulls.sh errors

--- pulls.sh
+++ pulls.sh
@@ -8,9 +8,7 @@ GIHUB_TOKEN=$(cat env.json | jq -r '.github_token')
 GITHUB_ORGANIZATION=$(cat env.json | jq  -r '.github_organization')
 GITHUB_REPOSITORY=$(cat env.json | jq  -r '.github_repository')
 
-mkdir -p "${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}"
-
 curl \
   -H "Authorization: token ${GIHUB_TOKEN}" \
-  "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/pulls/" \
-  > "${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/pulls.json"
+  "https://api.github.com/repos/${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}/pulls" \
+  > pulls.json
